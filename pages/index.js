fe95245af29ba4d3f1a70c56b2a9f2f1091130f5
ehak123 <73587185+ehak123@users.noreply.github.com>
2022-03-07 15:08:27 +0100

    latest

--- pages/index.js
+++ pages/index.js
@@ -49,12 +49,12 @@ export default function StartPageView({ allPostsData }) {
             <p>Welcome to Stockholm Speed Dating, your interactive and modern matchmaker-service!</p>
           </section>
           <section className={utilStyles.grid, utilStyles.container}>
-            <a href="" className={utilStyles.card} disabled={this}>
+            <a href="./login/account/register" className={utilStyles.card} disabled={this}>
               <h3>Become a member today &rarr;</h3>
               <p>Join our dating comunity and find your soulmate</p>
             </a>
 
-            <a href="./login" className={utilStyles.card}>
+            <a href="./login/account/login" className={utilStyles.card}>
               <h3>Log in &rarr;</h3>
               <p>Log in in order to sign up to new exiting events</p>
             </a>
