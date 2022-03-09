1047d3331a79746276415c7b96327f9824022772
ehak123 <73587185+ehak123@users.noreply.github.com>
2022-03-09 16:31:43 +0100

    some changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b7ed6945aed30347"; }
+/******/ 		__webpack_require__.h = function() { return "daa84fdf55c565d7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/login/account/login.jsx
+++ pages/login/account/login.jsx
@@ -2,10 +2,10 @@ import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
-
 import { Link } from 'logincomp';
 import { Layout } from 'logincomp/account';
 import { userService, alertService } from 'services';
+
 
 export default Login;
 
